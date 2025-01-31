0b797eb3e05c9559b5ccb08c877d74b3aa54dd2b
ahvvad <ahvadd.k@gmail.com>
2025-01-31 19:06:17 +0200

    add the local data

--- lib/screens/home/views/widgets/transactions_item.dart
+++ lib/screens/home/views/widgets/transactions_item.dart
@@ -1,5 +1,5 @@
-
 import 'package:flutter/material.dart';
+import 'package:money_tracker/data/data.dart';
 
 class TransactionsItem extends StatelessWidget {
   const TransactionsItem({
@@ -10,7 +10,7 @@ class TransactionsItem extends StatelessWidget {
   Widget build(BuildContext context) {
     return Expanded(
       child: ListView.builder(
-        itemCount: 6,
+        itemCount: transactionsData.length,
         itemBuilder: (context, int i) {
           return Padding(
             padding: const EdgeInsets.only(bottom: 16.0),
@@ -33,24 +33,23 @@ class TransactionsItem extends StatelessWidget {
                               width: 50,
                               height: 50,
                               decoration: BoxDecoration(
-                                color: Colors.yellow[700],
+                                color: transactionsData[i]['color'],
                                 shape: BoxShape.circle,
                               ),
                             ),
-                            const Icon(
-                              Icons.food_bank,
+                            Icon(
+                              transactionsData[i]['icon'],
                               color: Colors.white,
                             ),
                           ],
                         ),
                         const SizedBox(width: 12),
                         Text(
-                          'Food',
+                          transactionsData[i]['name'],
                           style: TextStyle(
                             fontSize: 16,
                             fontWeight: FontWeight.w500,
-                            color: const Color(0xFF263A4D)
-                                .withOpacity(0.9),
+                            color: const Color(0xFF263A4D).withOpacity(0.9),
                           ),
                         )
                       ],
@@ -59,21 +58,19 @@ class TransactionsItem extends StatelessWidget {
                       crossAxisAlignment: CrossAxisAlignment.end,
                       children: [
                         Text(
-                          '-\$45.00',
+                          transactionsData[i]['totalAmount'],
                           style: TextStyle(
                             fontSize: 16,
                             fontWeight: FontWeight.w400,
-                            color: const Color(0xFF263A4D)
-                                .withOpacity(0.9),
+                            color: const Color(0xFF263A4D).withOpacity(0.9),
                           ),
                         ),
                         Text(
-                          'Today',
+                          transactionsData[i]['date'],
                           style: TextStyle(
                             fontSize: 12,
                             fontWeight: FontWeight.w500,
-                            color: const Color(0xFF263A4D)
-                                .withOpacity(0.5),
+                            color: const Color(0xFF263A4D).withOpacity(0.5),
                           ),
                         ),
                       ],
