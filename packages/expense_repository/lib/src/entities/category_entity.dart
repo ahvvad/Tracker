6df63649cb1dda4d3ab4632d298b518c17c84a0a
ahvvad <ahvadd.k@gmail.com>
2025-02-03 16:12:17 +0200

    category logic done

---
+++ packages/expense_repository/lib/src/entities/category_entity.dart
@@ -0,0 +1,35 @@
+class CategoryEntity {
+  late String categoryId;
+  late String name;
+  late int totalExpenses;
+  late String icon;
+  late String color;
+  
+  CategoryEntity({
+    required this.categoryId,
+    required this.name,
+    required this.totalExpenses,
+    required this.icon,
+    required this.color,
+  });
+
+  Map<String, Object?> toDocument() {
+    return {
+      'categoryId': categoryId,
+      'name': name,
+      'totalExpenses': totalExpenses,
+      'icon': icon,
+      'color': color,
+    };
+  }
+
+  static CategoryEntity fromDocument(Map<String, dynamic> doc) {
+    return CategoryEntity(
+      categoryId: doc['categoryId'],
+      name: doc['name'],
+      totalExpenses: doc['totalExpenses'],
+      icon: doc['icon'],
+      color: doc['color'],
+    );
+  }
+}
---
+++ packages/expense_repository/lib/src/models/category.dart
@@ -0,0 +1,45 @@
+import '../entities/category_entity.dart';
+
+class Category {
+  late String categoryId;
+  late String name;
+  late int totalExpenses;
+  late String icon;
+  late String color;
+  
+  Category({
+    required this.categoryId,
+    required this.name,
+    required this.totalExpenses,
+    required this.icon,
+    required this.color,
+  });
+
+  static final empty = Category(
+    categoryId: '',
+    name: '',
+    totalExpenses: 0,
+    icon: '',
+    color: '',
+  );
+
+  CategoryEntity toEntity() {
+    return CategoryEntity(
+      categoryId: categoryId,
+      name: name,
+      totalExpenses: totalExpenses,
+      icon: icon,
+      color: color,
+    );
+  }
+
+  static Category fromEntity(CategoryEntity entity) {
+    return Category(
+      categoryId: entity.categoryId,
+      name: entity.name,
+      totalExpenses: entity.totalExpenses,
+      icon: entity.icon,
+      color: entity.color,
+    );
+  }
+}
